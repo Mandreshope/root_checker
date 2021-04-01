2ac178b934fb1c134cfaee297b47355ae8ab8e12
Mandresy Randrianarinjaka <mandreshope@gmail.com>
2021-04-01 21:28:44 +0300

    compileSdkVersion 29

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\flutter_windows_1.25.0-8.1.pre-beta"
+export "FLUTTER_APPLICATION_PATH=C:\Users\mandreshope\Documents\dev\FLUTTER PROJECT\root_checker"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_BUILD_NAME=0.1.3"
+export "FLUTTER_BUILD_NUMBER=0.1.3"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
